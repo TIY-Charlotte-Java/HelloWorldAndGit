8cd4a54de4fdc8763b5d33ee391946d6bc698b76
Ben Sterrett <raynjamin@gmail.com>
2016-03-26 22:10:46 -0400

    Re-adding better things.

---
+++ HelloWorldAndGit/test/com/ironyard/ben/sterrett/MainTest.java
@@ -0,0 +1,14 @@
+package com.ironyard.ben.sterrett;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ben on 3/26/16.
+ */
+public class MainTest {
+
+    @org.junit.Test
+    public void main() throws Exception {
+        Main.main(new String[0]);
+    }
+}
